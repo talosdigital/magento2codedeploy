a4dd510438a77828ecb04b86fe5af1280f55b4cf
ipascual <ignacio@talosdigital.com>
2018-08-31 17:20:14 -0500

    Update system_configure.sh

--- deployment/scripts/system_configure.sh
+++ deployment/scripts/system_configure.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 . /var/www/codedeploy/deployment/scripts/setenv.sh
 
-# Zabbix
-envsubst < $CODEDEPLOY/deployment/configs/zabbix_agentd.conf > /etc/zabbix/zabbix_agentd.conf
-systemctl restart zabbix-agent
