98031a38949bf96befded1ac2e00365e3f34a43f
ipascual <ignacio@talosdigital.com>
2018-08-31 17:35:33 -0500

    Update system_install.sh

--- deployment/scripts/system_install.sh
+++ deployment/scripts/system_install.sh
@@ -58,10 +58,12 @@ ln -sfn /opt/codedeploy-agent/deployment-root/deployment-logs/codedeploy-agent-d
 
 # Zabbix
 if [ ! -f "/etc/zabbix/zabbix_agentd.conf" ]; then
-    rpm -ivh http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
+    rpm -ivh https://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm
     yum -y install zabbix-agent
     firewall-cmd --zone=public --add-port=10050/tcp --permanent
     firewall-cmd --reload
+    systemctl start zabbix-agent
+    systemctl enable zabbix-agent
 fi
 
 # Remove any previous deployment
