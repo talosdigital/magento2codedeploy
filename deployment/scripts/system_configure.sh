575fb3931db15a768ca12964d7280df6c975f008
Daniel Martinez <daniel.martinez@talosdigital.com>
2018-09-13 15:55:42 -0500

    Adding custom php config

--- deployment/scripts/system_configure.sh
+++ deployment/scripts/system_configure.sh
@@ -4,3 +4,8 @@
 # Zabbix
 envsubst < $CODEDEPLOY/deployment/configs/zabbix_agentd.conf > /etc/zabbix/zabbix_agentd.conf
 systemctl restart zabbix-agent
+
+# PHP config
+if ! grep -q "post_max_size=64M" /etc/php.ini; then
+    echo "post_max_size=64M\nupload_max_filesize=64M\nmax_input_vars=750000" >> /etc/php.ini
+fi
