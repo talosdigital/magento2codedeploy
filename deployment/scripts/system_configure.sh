2109ee68be75c239c5fbdd9ef2a1bc29452a395d
Daniel Martínez <dmartinezmont@gmail.com>
2018-09-18 17:08:09 -0500

    Update system_configure.sh

--- deployment/scripts/system_configure.sh
+++ deployment/scripts/system_configure.sh
@@ -7,5 +7,7 @@ systemctl restart zabbix-agent
 
 # PHP config
 if ! grep -q "post_max_size=64M" /etc/php.ini; then
-    echo "post_max_size=64M\nupload_max_filesize=64M\nmax_input_vars=750000" >> /etc/php.ini
+    echo "post_max_size=64M" >> /etc/php.ini
+    echo "upload_max_filesize=64M" >> /etc/php.ini
+    echo "max_input_vars=750000" >> /etc/php.ini
 fi
