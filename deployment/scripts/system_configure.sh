bdbf84feb7c131999c71e4f636e0915ca013001a
Daniel Martinez <daniel.martinez@talosdigital.com>
2018-09-19 08:48:46 -0500

    Moving config to 90-talos.ini and moving file to specified folder

--- deployment/scripts/system_configure.sh
+++ deployment/scripts/system_configure.sh
@@ -5,9 +5,5 @@
 envsubst < $CODEDEPLOY/deployment/configs/zabbix_agentd.conf > /etc/zabbix/zabbix_agentd.conf
 systemctl restart zabbix-agent
 
-# PHP config
-if ! grep -q "post_max_size=64M" /etc/php.ini; then
-    echo "post_max_size=64M" >> /etc/php.ini
-    echo "upload_max_filesize=64M" >> /etc/php.ini
-    echo "max_input_vars=750000" >> /etc/php.ini
-fi
+# PHP
+/bin/cp $CODEDEPLOY/deployment/configs/90-talos.ini /etc/php.d/90-talos.ini
