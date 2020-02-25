362336a4d42c654357f7d3eed501853b74ab3d13
ipascual <ignacio@talosdigital.com>
2020-02-25 15:19:54 -0500

    Update system_configure.sh

--- deployment/scripts/system_configure.sh
+++ deployment/scripts/system_configure.sh
@@ -2,6 +2,9 @@
 . /var/www/codedeploy/deployment/scripts/setenv.sh
 figlet "System Configure"
 
+# Workaround 'sudo' long time
+sudo systemctl restart systemd-logind
+
 # Zabbix
 envsubst < $CODEDEPLOY/deployment/configs/zabbix_agentd.conf > /etc/zabbix/zabbix_agentd.conf
 systemctl restart zabbix-agent
