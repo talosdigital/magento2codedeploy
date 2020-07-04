40973a98ccd5802e1d745d6315ae294d957eb05f
Ignacio Pascual <ignacio@talosdigital.com>
2020-07-04 09:51:28 -0500

    only deployment mode

--- deployment/scripts/varnish_configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. /var/www/codedeploy/deployment/scripts/setenv.sh
-figlet "Varnish Configure"
-
-cat $CODEDEPLOY/varnish.vcl >> /etc/varnish/default.vcl || true
-
-systemctl reload varnish
